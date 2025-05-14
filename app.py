6c76c3a4224f246f45758418c2eb44472963e359
Salvador Reyna <salvadormizarreyna@gmail.com>
2025-05-13 19:31:38 -0500

    Agregado requirements.txt y Procfile

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+
+# Usa la variable de entorno DATABASE_URL de Render
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 # Usuario y contraseña predefinidos (puedes cambiarlos)
 USUARIO = 'vayavalla'
@@ -33,6 +41,18 @@ def movimientos():
 @app.route('/trabajos')
 def trabajos():
     return render_template('trabajos.html')
+
+# Clase Movimiento (debe estar fuera de las funciones de ruta)
+class Movimiento(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tipo = db.Column(db.String(20), nullable=False)
+    descripcion = db.Column(db.String(100))
+    cliente = db.Column(db.String(100))
+    fecha = db.Column(db.String(20))
+    mes = db.Column(db.String(20))
+    año = db.Column(db.String(10))
+    monto = db.Column(db.Float)
+    estado = db.Column(db.String(20))
 
 if __name__ == '__main__':
     app.run(debug=True)
